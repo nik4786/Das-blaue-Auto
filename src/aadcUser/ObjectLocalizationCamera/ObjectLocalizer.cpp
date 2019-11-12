6443a5c49d78fb2750ebed4f5be6fddcc0d38521
Florian Braun <thebraunber@googlemail.com>
2019-11-12 14:33:27 -0800

    Bugfixes

--- src/aadcUser/ObjectLocalizationCamera/ObjectLocalizer.cpp
+++ src/aadcUser/ObjectLocalizationCamera/ObjectLocalizer.cpp
@@ -28,23 +28,43 @@ tResult cObjectLocalizerCamera::ProcessInput(ISampleReader* pReader, const iobje
     output_sample_data<tObjectListPhys> output_sample(get_sample_time(pSample));
     output_sample->numResults = detection_results->numResults;
 
-    // const tObjectPhys* out_objects = reinterpret_cast<tObjectPhys*>(output_sample->objects);
-    memset(output_sample->objects, 0, sizeof(output_sample->objects)); // works because length of array objects is static
+    // set values in object list to a defined state (everything 0)
+    memset(output_sample->objects, 0, sizeof(output_sample->objects)); // works because length of array "objects" is static (see owndesc.h)
+
+    LOG_INFO("array size: %d",sizeof(output_sample->objects));
 
     for(uint8_t i=0;i<detection_results->numResults;++i)
     {
          strcpy(output_sample->objects[i].className, detection_results->objectsDetected[i].className);
          output_sample->objects[i].classificationProb = detection_results->objectsDetected[i].prob;
+
+        // propose a pose (rather bad because of low resolution but better than nothing?)
+        tObjectPose pose;
+        auto center_x = static_cast<uint16_t>(detection_results->objectsDetected[i].left+detection_results->objectsDetected[i].width/2);
+        auto bottom = static_cast<uint16_t>(detection_results->objectsDetected[i].top+detection_results->objectsDetected[i].height);
+        processForFrontCamera(center_x,bottom,pose);
+        output_sample->objects[i].pose = pose;
     }
 
+    m_writerObjectList->Write(output_sample.Release());
 
     RETURN_NOERROR;
 }
 
 void cObjectLocalizerCamera::processForFrontCamera(uint16_t x,uint16_t y,tObjectPose& result)
 {
-    result.x = 57.82/(y-473.9);
-    float angle = tan((-0.0002*x*x + 0.0185*x + 62.301)*DEG2RAD);
+    result.x = 57.82/(y-473.9); // calculate the x-coordinate in the sensor frame by the y-coordinate of the bbox in the image
+
+    // catch the case that the fit function is only valid for x<=625
+    float angle = 1;
+    if(x>625)
+    {
+        x = 1250-x;
+        angle = -1;
+    }
+
+    angle *= tan((-0.0002*x*x + 0.0185*x + 62.301)*DEG2RAD);
+
     result.y = result.x / angle;
 }
 void cObjectLocalizerCamera::processForRearCamera(uint16_t x,uint16_t y,tObjectPose& result)
