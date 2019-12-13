3b5cb504485308cf2ad64a51ad681600fe460dc9
nik4786 <56341784+nik4786@users.noreply.github.com>
2019-12-13 22:47:51 +0100

    Include extended comments

--- src/aadcTesting/Code/cpp2mat.m
+++ src/aadcTesting/Code/cpp2mat.m
@@ -1,14 +1,29 @@
 function cpp2mat(sourcepath, sourcefile, headerpath, headerfile)
+%% Setting Legacy Code Tool
 
+% initialize data structure to register characteristics of C++-Code and
+% properties of S-function 
 def = legacy_code('initialize');
+
+% defining path, in which the header and source file each is located
 def.IncPaths = {headerpath};
 def.SrcPaths = {sourcepath};
+
+% specify C++-Source and -Header filenames 
 def.SourceFiles = {sourcefile};
 def.HeaderFiles = {headerfile};
+
+% specify name and output function information of S-function
 def.SFunctionName = 'PIDcpp';
 def.OutputFcnSpec = 'double y1 = PIDController(double u1, double u2)';
+
+% specify the language of source file
 def.Options.language = 'C++';
+
+% generate S-function source file 
 legacy_code('sfcn_cmex_generate', def);
+
+% compile and link S-function source into dynamically loadable executable
 legacy_code('compile', def);
 
 %% Assign Simulink-Model to 'sys'
